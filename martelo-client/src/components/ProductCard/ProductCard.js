ca7702327ff353a41cf998e595be4aaaaba936dc
Roxanie Fernandes <roxaniedmfernandes@gmail.com>
2022-12-03 15:06:46 -0300

    feat: create component ProductCard

---
+++ martelo-client/src/components/ProductCard/ProductCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../ProductCard/ProductCard.css";
+import parafusadeiraSemFio from "../../img/parafusadeiraSemFio.jpeg";
+import { Link } from "react-router-dom";
+
+const ProductCard = () => {
+  // trocar nome da parafusadeira para nome do produto postado
+  const productName = "PARAFUSADEIRA";
+  return (
+    <div className="product-card">
+      {/* trocar imagem para banco de dados */}
+      <img className="logo" src={parafusadeiraSemFio} alt="martelo-logo"></img>
+      <p>{productName}</p>
+
+      {/* trocar para quando usuario estiver autenticado, direcionar para /reservar */}
+      <div className="rent-btn">
+        <Link to="/login">
+          <button>Alugar</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- martelo-client/src/pages/Home/Home.js
+++ martelo-client/src/pages/Home/Home.js
@@ -1,10 +1,23 @@
 import React from "react";
+import "../Home/Home.css"
 import Navbar from "../../components/Navbar/Navbar.js";
+import ProductCard from "../../components/ProductCard/ProductCard.js";
 
 const Home = () => {
   return (
     <div>
-      <Navbar />
+      <div>
+        <Navbar />
+      </div>
+      <div className="about-page">
+        <p>algo sobre a pagina algo sobre a pagina algo sobre a pagina algo sobre a pagina algo sobre a pagina algo sobre a pagina algo sobre a pagina algo sobre a pagina algo sobre a pagina algo sobre a pagina algo sobre a pagina algo sobre a pagina algo sobre a pagina algo sobre a pagina algo sobre a pagina</p>
+      </div>
+      <div className="first-products">
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+      </div>
     </div>
   );
 };
