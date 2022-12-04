45cd827827e251b574d45c27fb5cfdb2b7f195a8
Roxanie Fernandes <roxaniedmfernandes@gmail.com>
2022-12-04 11:18:47 -0300

    feat: create login form

---
+++ martelo-client/src/components/LoginForm/LoginForm.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { useState } from "react";
+import "../LoginForm/LoginForm.css";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+
+const LoginForm = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newLogin = {
+      email,
+      password,
+    };
+
+    await axios.post("link do deploy do server", newLogin);
+    navigate("/buscar-produto");
+  };
+
+  return (
+    <div className="newLogin">
+      <div className="form">
+        <h4>LOGIN</h4>
+        <form onSubmit={handleSubmit}>
+          <label>E-mail: </label>
+          <input
+            type="text"
+            required
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+
+          {/* arrumar para o passwordHash e verificação de senha | trocar a visualização da senha para pontinhos */}
+
+          <label>Senha: </label>
+          <input
+            type="text"
+            required
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+      <div className="createNewAccount">
+        <p>Ainda não tem uma conta? </p>
+        <ul>
+          <Link to="/cadastro" style={{ textDecoration: "none" }}>
+            <li>Clique aqui</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
--- martelo-client/src/pages/Login/Login.js
+++ martelo-client/src/pages/Login/Login.js
@@ -1,26 +1,33 @@
-import React from 'react'
-import "../Login/Login.css"
-import Navbar from '../../components/Navbar/Navbar'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "../Login/Login.css";
+import Navbar from "../../components/Navbar/Navbar";
+import { Link } from "react-router-dom";
+import LoginForm from "../../components/LoginForm/LoginForm.js";
 
 const Login = () => {
   return (
     <div>
-    <div>
-      <Navbar />
-    </div>
-    <div className="routes-in-page">
-      <ul>
-        <Link to="/" style={{ textDecoration: "none" }}>
-          <li className="home">HOME /</li>
-        </Link>
-        <Link to="/login" style={{ textDecoration: "none" }}>
-          <li className="home"> LOGIN</li>
-        </Link>
-      </ul>
-    </div>
-  </div>
-  )
-}
 
-export default Login
+      <div>
+        <Navbar />
+      </div>
+
+      <div className="routes-in-page">
+        <ul>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <li className="home">HOME /</li>
+          </Link>
+          <Link to="/login" style={{ textDecoration: "none" }}>
+            <li className="home"> LOGIN</li>
+          </Link>
+        </ul>
+      </div>
+
+      <div className="login-card">
+        <LoginForm/>        
+      </div>
+    </div>
+  );
+};
+
+export default Login;
