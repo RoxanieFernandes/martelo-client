67dd8be20b395c859acd01a33c8f98fea835428b
Roxanie Fernandes <roxaniedmfernandes@gmail.com>
2022-12-15 20:07:10 -0300

    first deploy

---
+++ martelo-client/src/components/Carousel/Carousel.js
@@ -0,0 +1,31 @@
+// import projectOne from "../../img/projectOne.jpeg";
+// import projectTwo from "../../img/projectTwo.jpeg";
+// import projectThree from "../../img/projectThree.jpeg";
+import "../Carousel/Carousel.css";
+
+const Carousel = () => {
+  return (
+    <div className="slideshow-container">
+      {/* <div className="mySlides-fade">
+        <img className="d-block w-100" src={projectOne} alt="First slide" />
+
+        <h3>Não deixe seu projeto para depois</h3>
+        <p>No MARTELO você encontra a ferramenta que precisa.</p>
+      </div>
+      <div className="mySlides-fade">
+        <img className="d-block w-100" src={projectTwo} alt="Second slide" />
+
+        <h3>Gosta de ferramentas?</h3>
+        <p>Seu equipamento parado pode te oferecer uma renda extra.</p>
+      </div>
+      <div className="mySlides-fade">
+        <img className="d-block w-100" src={projectThree} alt="Third slide" />
+
+        <h3>Third slide label</h3>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </div> */}
+    </div>
+  );
+};
+
+export default Carousel;
--- martelo-client/src/pages/Home/Home.js
+++ martelo-client/src/pages/Home/Home.js
@@ -1,35 +1,24 @@
 import React from "react";
 import "../Home/Home.css";
 import Navbar from "../../components/Navbar/Navbar.js";
-import ProductCard from "../../components/ProductCard/ProductCard.js";
+import Carousel from "../../components/Carousel/Carousel.js";
 
 const Home = () => {
   return (
     <div className="home">
-      <div>
+      <div className="navbar">
         <Navbar />
       </div>
-      <div className="about-page">
-        <p>
-          algo sobre a pagina algo sobre a pagina algo sobre a pagina algo sobre
-          a pagina algo sobre a pagina algo sobre a pagina algo sobre a pagina
-          algo sobre a pagina algo sobre a pagina algo sobre a pagina algo sobre
-          a pagina algo sobre a pagina algo sobre a pagina algo sobre a pagina
-          algo sobre a pagina
-        </p>
+      <div className="carousel">
+        <Carousel />
       </div>
-      <div className="first-products">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-      </div>
-      
+
       <div className="second-foot">
         <h1>Tem algum equipamento parado?</h1>
         <h3>Alguém pode estar procurando por ele.</h3>
       </div>
-    </div>
+
+       </div>
   );
 };
 
