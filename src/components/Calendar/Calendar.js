2f279f746ea4820fc6183b8291a660344e338034
Roxanie Fernandes <roxaniedmfernandes@gmail.com>
2022-12-16 23:41:11 -0300

    deploy fail

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -4,16 +4,14 @@ import { useNavigate } from "react-router-dom";
 import api from "../../service/api.service.js";
 
 const Calendar = (props) => {
-  const [owner, setOwner] = useState("");
-  const [renter, setRenter] = useState("");
-  const [product, setProduct] = useState("");
+  // const [owner, setOwner] = useState("");
+  // const [renter, setRenter] = useState("");
+  // const [product, setProduct] = useState("");
   const [inicialDate, setInicialDate] = useState("");
   const [deliveryDate, setDeliveryDate] = useState("");
-  const [price, setPrice] = useState("");
-  const [error, setError] = useState("");
-  const [message, setMessage] = useState("");
+  // const [price, setPrice] = useState("");
 
- 
+  const [message, setMessage] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -30,58 +28,65 @@ const Calendar = (props) => {
     try {
       console.log(newRent);
       await api.rent(newRent);
-      setMessage("Para combinar a retirada e pagamento, entre em contato com ", owner);
+      setMessage(
+        "Para combinar a retirada e pagamento, entre em contato com ",
+        owner
+      );
     } catch (error) {
       setError(error);
       console.log(error);
     }
   };
 
-  const showMessage = (message) => {
-    setMessage(message);
-    setTimeout(() => {
-      setMessage("");
-    }, 2000);
-  };
+  // const showMessage = (message) => {
+  //   setMessage(message);
+  //   setTimeout(() => {
+  //     setMessage("");
+  //   }, 2000);
+  // };
 
   return (
     <div>
-      <div className="message"> 
-      {message !== "" && <p>{message}</p>}
-      <div className="calendar">
-        <h4>FAÇA SUA RESERVA</h4>
-        <p>Selecione a data que deseja reservar a ferramenta 
-          e entre em contato com o locador.</p>
-        <form
-          onSubmit={(e) => {
-            handleSubmit(e);
-          }}
-        >
-          <label>Retirada</label>
-          <input
-            type="date"
-            required
-            value={inicialDate}
-            onChange={(e) => {
-              setInicialDate(e.target.value);
+      <div className="message">
+        {message !== "" && <p>{message}</p>}
+        <div className="calendar">
+          <h4>FAÇA SUA RESERVA</h4>
+          <p>
+            Selecione a data que deseja reservar a ferramenta e entre em contato
+            com o locador.
+          </p>
+          <form
+            onSubmit={(e) => {
+              handleSubmit(e);
             }}
-          />
+          >
+            <label>Retirada</label>
+            <input
+              type="date"
+              required
+              value={inicialDate}
+              onChange={(e) => {
+                setInicialDate(e.target.value);
+              }}
+            />
 
-          <label>Entrega</label>
-          <input
-            type="date"
-            required
-            value={deliveryDate}
-            onChange={(e) => {
-              setDeliveryDate(e.target.value);
-            }}
-          />
-          <div>
-          <button className="button" type="submit">Reservar</button>
-          </div>
-        </form>
+            <label>Entrega</label>
+            <input
+              type="date"
+              required
+              value={deliveryDate}
+              onChange={(e) => {
+                setDeliveryDate(e.target.value);
+              }}
+            />
+            <div>
+              <button className="button" type="submit">
+                Reservar
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
